baa52da9a4fcae1810f93742b12cc484dc25e76a
sagarbansal106 <sagar17bansal@gmail.com>
2019-10-26 15:15:42 +0000

    First Commit

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("ChartDemo.ChartDemo.controller.View1", {
+		onInit: function () {
+
+		}
+	});
+});
